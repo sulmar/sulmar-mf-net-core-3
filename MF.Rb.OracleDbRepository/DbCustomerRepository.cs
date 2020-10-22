782abbc631f29cd05360ba28ec91f449d5ef72d2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-10-22 08:30:44 +0200

    Dodanie metod obsługi string i int

--- MF.Rb.OracleDbRepository/DbCustomerRepository.cs
+++ MF.Rb.OracleDbRepository/DbCustomerRepository.cs
@@ -12,13 +12,54 @@ namespace MF.Rb.OracleDbRepository
     {
         public static DateTime? GetNullableDateTime(this OracleDataReader reader, string name)
         {
-            int col = reader.GetOrdinal(name);
-            return reader.GetNullableDateTime(col);
+            return reader.GetNullableDateTime(reader.GetOrdinal(name));
         }
         public static DateTime? GetNullableDateTime(this OracleDataReader reader, int col)
         {
-            return reader.IsDBNull(col) ? (DateTime?)null : (DateTime?)reader.GetDateTime(col);
+            return reader.IsDBNull(col) ? null : (DateTime?)reader.GetDateTime(col);
         }
+
+        public static string GetNullableString(this OracleDataReader reader, int col)
+        {
+            return reader.IsDBNull(col) ? null : reader.GetString(col);
+        }
+
+        public static string GetNullableString(this OracleDataReader reader, string name)
+        {
+            return reader.GetNullableString(reader.GetOrdinal(name));
+        }
+
+        public static short? GetNullableInt16(this OracleDataReader reader, int col)
+        {
+            return reader.IsDBNull(col) ? null : (short?)reader.GetInt16(col);
+        }
+
+        public static short? GetNullableInt16(this OracleDataReader reader, string name)
+        {
+            return reader.GetNullableInt16(reader.GetOrdinal(name));
+        }
+
+        public static int? GetNullableInt32(this OracleDataReader reader, int col)
+        {
+            return reader.IsDBNull(col) ? null : (int?) reader.GetInt32(col);
+        }
+
+        public static int? GetNullableInt32(this OracleDataReader reader, string name)
+        {
+            return reader.GetNullableInt32(reader.GetOrdinal(name));
+        }
+
+        public static long? GetNullableInt64(this OracleDataReader reader, int col)
+        {
+            return reader.IsDBNull(col) ? null : (long?)reader.GetInt64(col);
+        }
+
+        public static long? GetNullableInt64(this OracleDataReader reader, string name)
+        {
+            return reader.GetNullableInt64(reader.GetOrdinal(name));
+        }
+
+
     }
 
     // dotnet add package Oracle.ManagedDataAccess.Core
